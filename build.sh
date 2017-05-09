a123cd176630705080f21fb51b01bf87bf37608b
Xabier Larrakoetxea <slok69@gmail.com>
2017-05-09 19:57:52 +0200

    Don't compile statically in order to allow plugins to be loaded
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ F_VER="-X github.com/themotion/ladder/version.Version=${VERSION}"
 F_REV="-X github.com/themotion/ladder/version.Revision=${REVISION}"
 F_BR="-X github.com/themotion/ladder/version.Branch=${BRANCH}"
 F_DA="-X github.com/themotion/ladder/version.BuildDate=${DATE}"
-F_CMP="-w -linkmode external -extldflags '-static'"
+F_CMP="-w -linkmode external"
 
 go build -o ${OUT} --ldflags "${F_VER} ${F_REV} ${F_BR} ${F_DA} ${F_CMP}" ${SRC}
